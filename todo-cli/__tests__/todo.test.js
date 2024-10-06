34ebacb3777c112c110f11d9149a2c12e4391234
manish-107 <creativecrew2676@gmail.com>
2024-10-06 12:40:20 +0530

    todo-test

--- todo-cli/__tests__/todo.test.js
+++ todo-cli/__tests__/todo.test.js
@@ -24,51 +24,48 @@ describe("Todolist Test suite", () => {
       dueDate: new Date().toISOString().slice(0, 10),
     });
 
-    expect(all[0].completed).toBe(false); // Initially not completed
-    markAsComplete(0); // Mark the first todo as complete
-    expect(all[0].completed).toBe(true); // Now it should be marked as completed
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
   });
 
   test("retrieval of overdue items", () => {
-    // Add an overdue item (due date before today)
     add({
       title: "overdue test todo",
       completed: false,
       dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
         .toISOString()
-        .slice(0, 10), // Yesterday's date
+        .slice(0, 10),
     });
 
-    const overdueItems = overdue(); // Retrieve overdue items
-    expect(overdueItems.length).toBe(1); // We expect 1 overdue item
-    expect(overdueItems[0].title).toBe("overdue test todo"); // Ensure the correct item is overdue
+    const overdueItems = overdue();
+    expect(overdueItems.length).toBe(1);
+    expect(overdueItems[0].title).toBe("overdue test todo");
   });
 
   test("checks retrieval of due today items", () => {
-    // Add a todo item due today
     add({
       title: "due today items",
       completed: false,
-      dueDate: new Date().toISOString().slice(0, 10), // Today's date
+      dueDate: new Date().toISOString().slice(0, 10),
     });
 
-    const dueTodayItem = dueToday(); // Retrieve due today items
-    expect(dueTodayItem.length).toBe(1); // We expect 1 item due today
-    expect(dueTodayItem[0].title).toBe("due today items"); // Ensure the correct item is returned
+    const dueTodayItem = dueToday();
+    expect(dueTodayItem.length).toBe(1);
+    expect(dueTodayItem[0].title).toBe("due today items");
   });
 
   test("checks retrieval of due later items", () => {
-    // Add a todo item with a due date in the future (1 day later)
     add({
       title: "due later items",
       completed: false,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 1)) // Tomorrow's date
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
         .toISOString()
         .slice(0, 10),
     });
 
-    const dueLaterItem = dueLater(); // Retrieve due later items
-    expect(dueLaterItem.length).toBe(1); // We expect 1 item due later
-    expect(dueLaterItem[0].title).toBe("due later items"); // Ensure the correct item is returned
+    const dueLaterItem = dueLater();
+    expect(dueLaterItem.length).toBe(1);
+    expect(dueLaterItem[0].title).toBe("due later items");
   });
 });
